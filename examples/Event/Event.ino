91494af84f8bebb6c0e8066ae94f848f03911133
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2025-03-11 17:36:55 +0100

    Update Event.ino

--- examples/Event/Event.ino
+++ examples/Event/Event.ino
@@ -1,11 +1,11 @@
-#include "wtc801.h"
+#include "wtc401.h"
 
 #define SCK  D5
 #define MISO D6
 #define CS0  D8
 #define MOSI D7
 
-WTC801 panel(SCK, MOSI, MISO, CS0);
+WTC401 panel(SCK, MOSI, MISO, CS0);
 
 void onKeyEvent(KeyEvent event, uint8_t val) {
   switch (event) {
@@ -13,24 +13,32 @@ void onKeyEvent(KeyEvent event, uint8_t val) {
       Serial.print("Slider: ");
       Serial.println(val, DEC);
       break;
-    case KeyLeft:
-      Serial.print("KeyLeft: ");
+    case Key0:
+      Serial.print("Key0: ");
       if (val) {
         Serial.println("DOWN");
       } else {
         Serial.println("UP");
       }
       break;
-    case KeyPower:
-      Serial.print("KeyPower: ");
+    case Key1:
+      Serial.print("Key1: ");
       if (val) {
         Serial.println("DOWN");
       } else {
         Serial.println("UP");
       }
       break;
-    case KeyRight:
-      Serial.print("KeyRight: ");
+    case Key2:
+      Serial.print("Key2: ");
+      if (val) {
+        Serial.println("DOWN");
+      } else {
+        Serial.println("UP");
+      }
+      break;
+    case Key3:
+      Serial.print("Key3: ");
       if (val) {
         Serial.println("DOWN");
       } else {
